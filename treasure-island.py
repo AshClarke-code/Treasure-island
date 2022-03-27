444d1ab82471a30a8e17b33796a29bbf1bce1f81
AshClarke-code <64412678+AshClarke-code@users.noreply.github.com>
2022-03-27 16:03:10 +0200

    initial commit

---
+++ treasure-island.py
@@ -0,0 +1,217 @@
+# ###########################MESSAGES##############################
+opening_message = "Welcome to Treasure Island.\nYour mission is to find the treasure."
+
+chosen_path_fail = "You fell down a giant hole...\n... but, I couldn't find a picture for that, so it turned into a well instead..\n... I guess what I'm trying to say is you died!\nGAME OVER!!!!"
+chosen_path_success = "Oh wow... You made it.\nI'll be honest. I wasn't expecting that.\nSo...\non to the next one. I guess?"
+
+lake_message = "What a pretty lake.\n I mean really, I could just stare at it for hours...\n\n\n\n\n\n... what? Oh right?\n You're still here...\nfine."
+lake_decision_fail = "Wow!\nI mean you really need to learn to take a hint...\nStill going with that?\nfine\nHere's a fish...\n so pretty...\npretty enough to EAT YOU!\nYeah! It wasn't even a cool fish either...\nYou died!\nGAME OVER!!!"
+lake_decision_success = "You know? I'm really starting to like you.\n You're going to be really excited when you get to the end of this.\nI mean... good things really do come to those who wait."
+
+door_message = "Suddenly. Three magical doors appear.\nAlong with a very cute lil poody tat.\nHe's so fluffy!!!\nFOCUS BEAN!\nRight! You've come so far...\n it would be a shame for you to mess it all up now."
+door_failure_beast = "We were rooting for you!\nWe were all rooting for you!\nWhat even is that? It's absolutely terrifying.\nYou're on your own mate... I tried to warn you.\nYou were eaten by a beast named Chad.\nGAME OVER!"
+door_failure_fire = "You were the chosen one!\n You were supposed to bring balalance to the...\nOh damn! Guess now you're just fresh, fried and crispy.\nYou were burned by fire.\nGAME OVER!"
+door_success = "Oh mer gerd!\nYou did it. You actually did it!\nIt's been so long... I never thought anyone would ever actually find it.\nCongratulations! It's the EYE OF THE TIGER!\nVery rare. Very powerful.\nKind of gross!"
+
+exit_message = "This game is so silly... But it was so much fun to make. If you did actually try this. Thanks for playing and remember that code is supposed to be fun.\nYou won!"
+
+
+
+####################################IMAGES###########################
+treasure_chest = '''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+'''
+
+well = '''                 __
+               .'/\'.
+             .'-/__\-'.
+           .'--/____\--'.
+         .'--./______\.--'.
+       .'--../________\..--'.
+     .'--.._/__________\_..--'.
+   .'--..__/____________\__..--'.
+ .'--..___/______________\___..--'.
+'========'================'========'
+      [_|__]            [_|__]
+     =[__|_]=====""=====[__|_]==.
+      [_|__]     '|     [_|__]  |
+      [__|_]     |'     [__|_]  |
+      [_|__]  .--JL--.  [_|__]  '===O
+      [__|_]   \====/   [__|_]
+      [_|__]_.-| .; |-._[_|__]
+      [__|_]'._ \__/(_.'[__|_]
+      [.-._]            [_.-.]
+      [_.-.'--..____..--'.-._]
+ (o)  [(_.'   .-.     .-.'._)\ (o)
+(\'/) [  .-. (_.'.-. (_.' .-.](\'/)
+   ;: [ (_.'.-. (_.' .-. (_.'| ;:'
+;:    [ .-. '._) .-. '._).-. ]   ;:.
+      [(_.'  .-. '._) .-.'._)]
+  (o) /.-.  (_.'.-.  (_.' .-.];:(o)
+ (\'/)['._).-. (_.'   .-.(_.'] (\'/)
+      [   (_.'.-.  .-.'._)   \ ;:
+;:'   '-._    '._) '._)   _.-'
+  LGB      `---..____..---'   ;:`
+   ;:'      ;:'.:;     ;;"
+'''
+
+lake = '''                  _
+             .''.' \    _  __
+ ___         './    '. ' `'  `
+    '._______.'       \
+                       '.__________
+                                   '-.____________
+ _________________________________________________'.__________________
+                                      ____________.'
+                         __________.-'
+      _______          .'                      
+ ___.'       '.       /               '-._         
+             .'\    .' ._,.__,        ____\____.o.
+             '..'._/                 '-._______.-'
+                                     .-'_______'-.
+                                         _/    'o'
+                                      .-'
+                                      '''
+
+fish = '''
+                     __,
+               .-'_-'`
+             .' {`
+         .-'````'-.    .-'``'.
+       .'(0)       '._/ _.-.  `\
+      }     '. ))    _<`    )`  |
+       `-.,\'.\_,.-\` \`---; .' /
+            )  )       '-.  '--:
+           ( ' (          ) '.  \
+            '.  )      .'(   /   )
+              )/      (   '.    /
+                       '._( ) .'
+                    jgs    ( (
+                            `-.
+                            '''
+
+door =  '''           ________
+             / ______ \
+             || _  _ ||
+             ||| || |||
+             |||_||_|||
+             || _  _o|| (o)
+             ||| || |||
+             |||_||_|||      ^~^  ,
+             ||______||     ('Y') )
+            /__________\    /   \/
+    ________|__________|__ (\|||/) _________
+   hjw     /____________\
+   `97     |____________|
+   '''
+beast = '''
+                   (    )
+                  ((((()))
+                  |o\ /o)|
+                  ( (  _')
+                   (._.  /\__
+                  ,\___,/ '  ')
+    '.,_,,       (  .- .   .    )
+     \   \\     ( '        )(    )
+      \   \\    \.  _.__ ____( .  |
+       \  /\\   .(   .'  /\  '.  )
+        \(  \\.-' ( /    \/    \)
+         '  ()) _'.-|/\/\/\/\/\|
+             '\\ .( |\/\/\/\/\/|
+               '((  \    /\    /
+               ((((  '.__\/__.')
+                ((,) /   ((()   )
+                 "..-,  (()("   /
+            pils  _//.   ((() ."
+          _____ //,/" ___ ((( ', ___
+                           ((  )
+                            / /
+                          _/,/'
+                        /,/,"
+'''
+
+fire = '''
+               (  .      )
+           )           (              )
+                 .  '   .   '  .  '  .
+        (    , )       (.   )  (   ',    )
+         .' ) ( . )    ,  ( ,     )   ( .
+      ). , ( .   (  ) ( , ')  .' (  ,    )
+     (_,) . ), ) _) _,')  (, ) '. )  ,. (' )
+ ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+'''
+
+tiger = '''
+                         __,,,,_
+          _ __..-;''`--/'/ /.',-`-.
+      (`/' ` |  \ \ \\ / / / / .-'/`,_
+     /'`\ \   |  \ | \| // // / -.,/_,'-,
+    /<7' ;  \ \  | ; ||/ /| | \/    |`-/,/-.,_,/')
+   /  _.-, `,-\,__|  _-| / \ \/|_/  |    '-/.;.\'
+   `-`  f/ ;      / __/ \__ `/ |__/ |
+        `-'      |  -| =|\_  \  |-' |
+              __/   /_..-' `  ),'  //
+          fL ((__.-'((___..-'' \__.'
+'''
+
+
+
+###########################GAME######################################
+print(treasure_chest)
+print(opening_message)
+
+chosen_path = input("You arrive at a crossroads. Which path do you take? Type 'left' or 'right':\n")
+if chosen_path != "left":
+    print(well)
+    print(chosen_path_fail)
+    exit()
+
+print(chosen_path_success)
+print(lake)
+print(lake_message)
+
+lake_decsion = input("You've come across the most beautiful lake it the world. Will you wait here or swim across? Type 'swim' or 'wait':\n")
+if lake_decsion != "wait":
+    print(fish)
+    print(lake_decision_fail)
+    exit()
+
+print(lake_decision_success)
+print(door)
+print(door_message)
+
+door_decision = input("This is it. Three doors. Blue, Red and Yellow, what will it be? Type 'blue', 'red' or 'yellow':\n")
+if door_decision == "blue":
+  print(beast)
+  print(door_failure_beast)
+  exit()
+if door_decision =="red":
+  print(fire)
+  print(door_failure_fire)
+  exit()
+if door_decision == "yellow":
+  print(tiger)
+  print(door_success)
+  print(exit_message)
+  exit()
+print("I'm not even going to dignify that with a response.\n GAME OVER.\n")
+exit()
